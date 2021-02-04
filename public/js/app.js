fc4845e2f37626f62947a7ef0c0c87b08ad459f0
Esin Adsoy <esin.adsoy@gmail.com>
2021-02-04 15:10:19 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log('Client side js file is loaded!');
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   })
+// })
+
+const getForecast = (address) => {
+  const url = `http://localhost:3000/weather?address=${address}`
+
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
+  })
+}
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+  getForecast(location);
+})
